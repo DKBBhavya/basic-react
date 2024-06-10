75d5038e466e8ac7542c9801cad3ac6bd230f5a3
Bhavya Agarwal <bhavya.dkb@gmail.com>
2024-06-10 21:56:39 +0530

    implemented virtual dom

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 // Library
+const React = {
+    createElement: (tag, props, ...children) => {
+        if (typeof tag === 'function') {
+            return tag(props, ...children);
+        }
+        const el = {
+            tag,
+            props,
+            children,
+        };
+        return el;
+    },
+};
+const render = (el, container) => {
+    let domEl;
+    if (typeof el === 'string') {
+        domEl = document.createTextNode(el);
+        container.appendChild(domEl);
+        return;
+    }
+    domEl = document.createElement(el.tag);
+    let elProps = el.props ? Object.keys(el.props) : null;
+    if (elProps && elProps.length > 0) {
+        elProps.forEach((prop) => (domEl[prop] = el.props[prop]));
+    }
+    if (el.children && el.children.length > 0) {
+        el.children.forEach((node) => render(node, domEl));
+    }
+    container.appendChild(domEl);
+};
 // Application
-console.log("Hello World");
+const App = () => {
+    return (React.createElement("div", { draggable: true },
+        React.createElement("h2", null, "Hello React!"),
+        React.createElement("p", null, "I am a pargraph"),
+        React.createElement("input", { type: "text" })));
+};
+render(React.createElement(App, null), document.getElementById('myapp'));
--- app.tsx
+++ app.tsx
@@ -1,5 +1,50 @@
 // Library
+const React = {
+  createElement: (tag, props, ...children) => {
+    if (typeof tag === 'function') {
+      return tag(props, ...children);
+    }
+    const el = {
+      tag,
+      props,
+      children,
+    };
+    return el;
+  },
+};
+
+const render = (el, container) => {
+  let domEl;
+
+  if (typeof el === 'string') {
+    domEl = document.createTextNode(el);
+    container.appendChild(domEl);
+    return;
+  }
+
+  domEl = document.createElement(el.tag);
+
+  let elProps = el.props ? Object.keys(el.props) : null;
+  if (elProps && elProps.length > 0) {
+    elProps.forEach((prop) => (domEl[prop] = el.props[prop]));
+  }
+
+  if (el.children && el.children.length > 0) {
+    el.children.forEach((node) => render(node, domEl));
+  }
+
+  container.appendChild(domEl);
+};
 
 
 // Application
-console.log("Hello World")
+const App = () => {
+  return (
+    <div draggable>
+      <h2>Hello React!</h2>
+      <p>I am a pargraph</p>
+      <input type="text" />
+    </div>
+  );
+};
+render(<App />, document.getElementById('myapp'));
